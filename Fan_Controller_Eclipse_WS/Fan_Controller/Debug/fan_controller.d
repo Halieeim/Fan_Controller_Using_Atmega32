583ae235e6b9280a9c66dd98898bffdf60c376e0
Halieeim <ahmedlpl86@gmail.com>
2022-10-18 20:17:48 +0200

    Initial Commit

---
+++ Fan_Controller_Eclipse_WS/Fan_Controller/Debug/fan_controller.d
@@ -0,0 +1,12 @@
+fan_controller.o fan_controller.o: ../fan_controller.c ../lcd.h \
+  ../std_types.h ../lm35_sensor.h ../DcMotor.h ../adc.h
+
+../lcd.h:
+
+../std_types.h:
+
+../lm35_sensor.h:
+
+../DcMotor.h:
+
+../adc.h:
